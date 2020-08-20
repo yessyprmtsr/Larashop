164f3de84cf6129c17f60bf19dde5e32e511bb5b
yessy <yessypermatasari8@gmail.com>
2020-08-20 10:05:15 +0700

    first commit+create migrate categories

---
+++ database/migrations/2020_08_20_020802_create_catagories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatagoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catagories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            //untuk seo firendly
+            $table->string('slug');
+            //untuk multi level category buat induk
+            $table->bigInteger('parent_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catagories');
+    }
+}
